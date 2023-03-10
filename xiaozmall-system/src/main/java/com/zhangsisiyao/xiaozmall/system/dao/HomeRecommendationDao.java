dc1d405b00f3634d83f05d0ac18ebdfb84d7fdae
mc23 <2963456487@qq.com>
2023-03-10 16:23:40 +0800

    新建商城用户页面

---
+++ xiaozmall-system/src/main/java/com/zhangsisiyao/xiaozmall/system/dao/HomeRecommendationDao.java
@@ -0,0 +1,17 @@
+package com.zhangsisiyao.system.system.dao;
+
+import com.zhangsisiyao.system.system.entity.HomeRecommendationEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 
+ * 
+ * @author system
+ * @email 2963456487@qq.com
+ * @date 2023-03-10 16:22:36
+ */
+@Mapper
+public interface HomeRecommendationDao extends BaseMapper<HomeRecommendationEntity> {
+	
+}
